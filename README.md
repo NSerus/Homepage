3ba4b4e8fd9d39f60c89ffd1ab4b4a8cbcd76d8a
NSerus <nserus@pop-os.localdomain>
2023-12-19 20:29:42 +0000

    Added clock code

--- README.md
+++ README.md
@@ -68,3 +68,8 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+# Intro to app
+
+This React app will be a fully decked homepage with a clock, calendar, todo list and pomodoro to do all your work
--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [time, setTime] = useState('');
+
+  function formatTime(val){
+    if (val < 10) {
+      return '0';
+    }else{
+      return '';
+    }
+  }
+  
+  useEffect(() => {
+    const timerID = setInterval(
+      () => tick(), 1000
+    )
+    return function cleanup(){
+      clearInterval(timerID);
+    }
+  })
+  
+  function tick() {
+    const d = new Date();
+    const h = d.getHours();
+    const m = d.getMinutes();
+    const s = d.getSeconds();
+  
+    setTime(formatTime(h) + h + ':' + formatTime(m) + m + ':'+ formatTime(s) + s)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,9 +47,12 @@ function App() {
         >
           Learn React
         </a>
+        <div>{time}</div>
       </header>
     </div>
   );
 }
+
+
 
 export default App;
