99eb3eb15370cae98788e147223b86af0e32f359
NSerus <nunoworkingserus@gmail.com>
2023-12-25 18:27:01 +0000

    Removed initial bloat

--- src/App.js
+++ src/App.js
@@ -1,58 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [time, setTime] = useState(""); //useState to have a value that can be updated
 
-  const [time, setTime] = useState('');
-
-  function formatTime(val){
+  function formatTime(val) {
+    // to keep the '00:00:00' format instead of being like 0:0:01
     if (val < 10) {
-      return '0';
-    }else{
-      return '';
+      return "0";
+    } else {
+      return "";
     }
   }
-  
+
   useEffect(() => {
-    const timerID = setInterval(
-      () => tick(), 1000
-    )
-    return function cleanup(){
+    const timerID = setInterval(() => tick(), 1000);
+    return function cleanup() {
       clearInterval(timerID);
-    }
-  })
-  
+    };
+  });
+
   function tick() {
+    // function for each tick
     const d = new Date();
     const h = d.getHours();
     const m = d.getMinutes();
     const s = d.getSeconds();
-  
-    setTime(formatTime(h) + h + ':' + formatTime(m) + m + ':'+ formatTime(s) + s)
+
+    setTime(
+      formatTime(h) + h + ":" + formatTime(m) + m + ":" + formatTime(s) + s
+    ); // sets all the values to the constant to put on DOM
   }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>{time}</div>
-      </header>
-    </div>
+    <header className="App-header">
+      <div>{time}</div>
+    </header>
   );
 }
-
-
 
 export default App;
