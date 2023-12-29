2e4ecde929daa3f51256a7da6ceb3f8b1573eb09
NSerus <nserus@pop-os.localdomain>
2023-12-29 15:26:57 +0000

    styling and React organizing

--- src/App.js
+++ src/App.js
@@ -1,41 +1,23 @@
 import "./App.css";
+import Clock from './components/Clock';
+import Tasks from './components/Tasks';
+import Weather from './components/Weather';
+import Pomo from './components/Pomo';
 import { useEffect, useState } from "react";
 
 function App() {
-  const [time, setTime] = useState(""); //useState to have a value that can be updated
-
-  function formatTime(val) {
-    // to keep the '00:00:00' format instead of being like 0:0:01
-    if (val < 10) {
-      return "0";
-    } else {
-      return "";
-    }
-  }
-
-  useEffect(() => {
-    const timerID = setInterval(() => tick(), 1000);
-    return function cleanup() {
-      clearInterval(timerID);
-    };
-  });
-
-  function tick() {
-    // function for each tick
-    const d = new Date();
-    const h = d.getHours();
-    const m = d.getMinutes();
-    const s = d.getSeconds();
-
-    setTime(
-      formatTime(h) + h + ":" + formatTime(m) + m + ":" + formatTime(s) + s
-    ); // sets all the values to the constant to put on DOM
-  }
+  
 
   return (
-    <header className="App-header">
-      <div>{time}</div>
-    </header>
+    <div>
+        <Clock/>
+
+      <div className="apps"> 
+        <Tasks/>
+        <Weather/>
+      </div>
+      <Pomo/>
+    </div>
   );
 }
 
