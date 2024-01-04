c98db27e6662acbaafb411b1d7b4985f0fc2b9d2
NSerus <nunoworkingserus@gmail.com>
2024-01-04 20:57:51 +0000

    Added CurrentWeather functionality

--- src/components/Weather/api.js
+++
@@ -1,9 +0,0 @@
-export const geoApiUrl = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities';
-export const geoApiOptions = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "dc4afedd78msh5b5ca6b9e251468p19630djsnaa655df7f867",
-    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
-  },
-};
-
